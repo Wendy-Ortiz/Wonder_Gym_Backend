19cee289bf0de76220d4594e6b523b2335d209f0
AxelMatus <axelantonio@gmail.com>
2022-07-03 21:55:27 -0600

    Added new validation middleware and changes in authorization middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,15 +1,44 @@
+const jwt = require("jsonwebtoken");
+const data = require('../data.js');
 const { ROLES } = require("../utils/constants");
 
 exports.checkUserIsAuthenticated = async (req, res, next) => {
-    const userIsAuthenticated = true;
-    if(!userIsAuthenticated) {
-        return res.status(401).json({
-            error: true,
-            message: "El usuario no esta autenticado",
-        });
+    let token;
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        token = req.headers.authorization.split(" ")[1];
+        if (!token) {
+            res.status(401).json({
+                error: true,
+                message: "El usuario no está autenticado",
+            });
+        }
+        else {
+            try {
+                const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+                const user = (data.all_Users).filter(user => user.id === decodedToken.userId);
+                if (!user) {
+                    res.status(401).json({
+                        error: true,
+                        message: "El usuario no está autenticado",
+                    });
+                }
+                req.userRoles = decodedToken.roles;
+                next();
+            } catch (error) {
+                res.status(400).json({
+                    error: true,
+                    message: "La información enviada no es válida. Verifique que su sesión no haya expirado.",
+                });
+            }
+        }
     }
-    next();
-}
+    else {
+        res.status(401).json({
+            error: true,
+            message: "El usuario no está autenticado",
+        });
+    }  
+};
 
 exports.checkRoles = (requiredRoles) => {
     return async (req, res, next) => {
